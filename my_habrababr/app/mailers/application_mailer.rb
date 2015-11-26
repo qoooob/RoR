22d78a7b57c6f55ac681eb2e1046fb5b0166e242
Aleksey Kubarev <aleksey.kubarev@gmail.com>
2015-11-26 21:24:32 +0300

    <MAILGUN config>

--- my_habrababr/app/mailers/application_mailer.rb
+++ my_habrababr/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "mail@app391a0fc051ba417ca9ec48207cc67d58.mailgun.org"
   layout 'mailer'
 end
--- my_habrababr/config/initializers/devise.rb
+++ my_habrababr/config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'mail@app391a0fc051ba417ca9ec48207cc67d58.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
